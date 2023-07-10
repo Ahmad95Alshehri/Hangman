fb669366cbf4e7952900b3e716bb07355d36293e
Ahmad Zafer Alshehri <ahmad.z.95@hotmail.com>
2023-07-10 22:11:55 +0300

    Hangman Game

---
+++ assig2/assig2/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace assig2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.picHangman = new System.Windows.Forms.PictureBox();
+            this.Guess = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblScore = new System.Windows.Forms.TextBox();
+            this.lblNewWord = new System.Windows.Forms.TextBox();
+            this.cmbWordFiles = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picHangman)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Location = new System.Drawing.Point(365, 105);
+            this.txtGuess.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(264, 22);
+            this.txtGuess.TabIndex = 0;
+            // 
+            // picHangman
+            // 
+            this.picHangman.Location = new System.Drawing.Point(365, 205);
+            this.picHangman.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.picHangman.Name = "picHangman";
+            this.picHangman.Size = new System.Drawing.Size(282, 230);
+            this.picHangman.TabIndex = 1;
+            this.picHangman.TabStop = false;
+            // 
+            // Guess
+            // 
+            this.Guess.Location = new System.Drawing.Point(788, 105);
+            this.Guess.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Guess.Name = "Guess";
+            this.Guess.Size = new System.Drawing.Size(180, 53);
+            this.Guess.TabIndex = 2;
+            this.Guess.Text = "Guess Letter";
+            this.Guess.UseVisualStyleBackColor = true;
+            this.Guess.Click += new System.EventHandler(this.Guess_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(465, 532);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 56);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Quit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblScore
+            // 
+            this.lblScore.Location = new System.Drawing.Point(48, 316);
+            this.lblScore.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(169, 22);
+            this.lblScore.TabIndex = 6;
+            this.lblScore.TextChanged += new System.EventHandler(this.lblScore_TextChanged);
+            // 
+            // lblNewWord
+            // 
+            this.lblNewWord.Location = new System.Drawing.Point(48, 404);
+            this.lblNewWord.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lblNewWord.Name = "lblNewWord";
+            this.lblNewWord.Size = new System.Drawing.Size(169, 22);
+            this.lblNewWord.TabIndex = 7;
+            this.lblNewWord.TextChanged += new System.EventHandler(this.lblNewWord_TextChanged);
+            // 
+            // cmbWordFiles
+            // 
+            this.cmbWordFiles.FormattingEnabled = true;
+            this.cmbWordFiles.Location = new System.Drawing.Point(743, 411);
+            this.cmbWordFiles.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbWordFiles.Name = "cmbWordFiles";
+            this.cmbWordFiles.Size = new System.Drawing.Size(257, 24);
+            this.cmbWordFiles.TabIndex = 9;
+            this.cmbWordFiles.Text = "cmbWordFiles";
+            this.cmbWordFiles.SelectedIndexChanged += new System.EventHandler(this.cmbWordFiles_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1063, 774);
+            this.Controls.Add(this.cmbWordFiles);
+            this.Controls.Add(this.lblNewWord);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Guess);
+            this.Controls.Add(this.picHangman);
+            this.Controls.Add(this.txtGuess);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picHangman)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.PictureBox picHangman;
+        private System.Windows.Forms.Button Guess;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox lblScore;
+        private System.Windows.Forms.TextBox lblNewWord;
+        private System.Windows.Forms.ComboBox cmbWordFiles;
+    }
+}
+
---
+++ assig2/assig2/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace assig2
+{
+    public partial class Form1 : Form
+    {
+        private int currentStage = 0;
+        private string correctWord = "";
+        private int score = 0;
+        private int Lscore = 0;
+        private bool gameEnded = false;
+        private List<Label> incorrectLabels = new List<Label>();
+        private string[] wordFiles = { "C:\\Users\\Ahmad Alshehri\\Desktop\\LIU CLASSES\\spring 2023\\assig2\\words1.txt",
+            "C:\\Users\\Ahmad Alshehri\\Desktop\\LIU CLASSES\\spring 2023\\assig2\\words2.txt" };
+
+
+
+
+        private Image[] hangmanImages = new Image[]//array of images load from resources
+        {
+            assig2.Resource1.hang1,assig2.Resource1.hang2,assig2.Resource1.hang3,assig2.Resource1.hang4,assig2.Resource1.hang5,assig2.Resource1.hang6,assig2.Resource1.hang7,
+        };
+        public Form1()
+        {
+            InitializeComponent();
+            // Add word files to the ComboBox
+            cmbWordFiles.Items.AddRange(wordFiles);
+
+            // Select the first word file by default
+            cmbWordFiles.SelectedIndex = 0;
+            StartNewGame();
+        }
+
+        private void StartNewGame()
+        {
+            // remove old labels
+            for (int i = 1; i <= correctWord.Length; i++)
+            {
+                Control control = this.Controls.Find("lblLetter" + i, true)[0];
+                this.Controls.Remove(control);
+            }
+
+            foreach (Label incorrectLabel in incorrectLabels)
+            {
+                this.Controls.Remove(incorrectLabel);
+            }
+            incorrectLabels.Clear();
+
+
+            // Select a new random word from the selected word file
+            string[] allWords = File.ReadAllLines(cmbWordFiles.SelectedItem.ToString());
+            Random rand = new Random();
+            correctWord = allWords[rand.Next(allWords.Length)];
+
+      
+
+
+            // create the label controls for the word
+            for (int i = 0; i < correctWord.Length; i++)
+            {
+                Label label = new Label();
+                label.Name = "lblLetter" + (i + 1);
+                label.Text = "_";
+                label.AutoSize = false;
+                label.Width = 25;
+                label.TextAlign = ContentAlignment.MiddleCenter;
+                label.Font = new Font("Arial", 16);
+                label.Location = new Point(50 + (i * 25), 150);
+                this.Controls.Add(label);
+            }
+
+            gameEnded = false;
+
+            // reset pic
+            currentStage = 0;
+            picHangman.Image = hangmanImages[currentStage];
+
+            // chnage   labels  won or lost
+            if (currentStage == 6)
+            {
+                MessageBox.Show("You lost! The correct word was " + correctWord);
+            }
+            else if (correctWord == string.Join("", this.Controls.OfType<Label>().Where(l => l.Name.StartsWith("lblLetter")).Select(l => l.Text)))
+            {
+                MessageBox.Show("You won!");
+            }
+        }
+
+        private void InitializeWordLabels(int wordLength)
+        {
+
+            string[] words1 = File.ReadAllLines("C:\\Users\\Ahmad Alshehri\\Desktop\\LIU CLASSES\\spring 2023\\assig2\\words1.txt");
+            string[] words2 = File.ReadAllLines("C:\\Users\\Ahmad Alshehri\\Desktop\\LIU CLASSES\\spring 2023\\assig2\\words2.txt");
+
+            // put togather the words from both files
+            string[] allWords = words1.Concat(words2).ToArray();
+
+            // select a random word
+            Random rand = new Random();
+            correctWord = allWords[rand.Next(allWords.Length)];
+
+            // update the score counter
+            lblScore.Text = "Score: " + score.ToString();
+
+
+            // create the label controls for the word
+            for (int i = 0; i < correctWord.Length; i++)
+            {
+                Label label = new Label();
+                label.Name = "lblLetter" + (i + 1);
+                label.Text = "_";
+                label.AutoSize = false;
+                label.Width = 25;
+                label.TextAlign = ContentAlignment.MiddleCenter;
+                label.Font = new Font("Arial", 16);
+                label.Location = new Point(50 + (i * 25), 150);
+                this.Controls.Add(label);
+            }
+        }
+
+
+        //returns the label control that matches the given index
+        private Label GetMatchingLabel(int index)
+        {
+            string labelName = "lblLetter" + (index + 1);
+            return (Label)this.Controls.Find(labelName, true)[0];
+        }
+
+        //updates the labels to display the complete word.
+        private void DisplayWord(string word)
+        {
+            // show the complete word in the labels
+            for (int i = 0; i < word.Length; i++)
+            {
+                Label label = GetMatchingLabel(i);
+                label.Text = word[i].ToString();
+            }
+        }
+
+
+        private void Guess_Click(object sender, EventArgs e)
+        {
+            if (gameEnded)
+            {
+                StartNewGame();
+                return;
+            }
+
+            string guess = txtGuess.Text.Trim().ToUpper();
+
+            // check if the guess is a single letter
+            if (guess.Length == 1)
+            {
+                bool letterFound = false;
+
+                // check if the guess is in the word and update the labels
+                for (int i = 0; i < correctWord.Length; i++)
+                {
+                    if (correctWord[i].ToString().ToUpper() == guess)
+                    {
+                        Label label = GetMatchingLabel(i);
+                        label.Text = correctWord[i].ToString();
+                         label.ForeColor = Color.Green;
+                        letterFound = true;
+                    }
+                   
+                }
+                if (guess.Length > 1)
+                {
+                    MessageBox.Show("please enter one letter");
+                }
+
+
+                // if the letter is not in the word, update the hangman image and incorrect guesses label
+                if (!letterFound)
+                {
+                    currentStage++;
+                    picHangman.Image = hangmanImages[currentStage];
+                   
+                   
+                    Label incorrectLabel = new Label();
+                    incorrectLabel.Text = guess;
+                    incorrectLabel.AutoSize = true;
+                    incorrectLabel.Font = new Font("Arial", 12);
+                    incorrectLabel.ForeColor = Color.Red;
+                    incorrectLabel.Location = new Point(50 + (incorrectLabels.Count * 20), 200);
+                    this.Controls.Add(incorrectLabel);
+                    incorrectLabels.Add(incorrectLabel);
+
+                    if (currentStage == 6)
+                    {
+                        MessageBox.Show("You lost! The correct word was " + correctWord);
+                        gameEnded = true;
+                        Lscore += 1;
+                        lblNewWord.Text = "Losses: " + Lscore.ToString();
+                       
+                        return;
+                    }
+                }
+
+                // check if the player has won
+                if (correctWord == string.Join("", this.Controls.OfType<Label>().Where(l => l.Name.StartsWith("lblLetter")).Select(l => l.Text)))
+                {
+                    MessageBox.Show("You won!");
+                    gameEnded = true;
+                    score += 1;
+                    lblScore.Text = "Score: " + score.ToString();
+                   
+                    return;
+                }
+            }
+            else if (guess.Length == correctWord.Length)
+            {
+                // check if the guess is equal to the correct word
+                if (guess == correctWord.ToUpper())
+                {
+                    DisplayWord(correctWord.ToUpper());
+                    MessageBox.Show("You won!");
+                    gameEnded = true;
+                    score += 1;
+                    lblScore.Text = "Score: " + score.ToString();
+                
+                    return;
+                }
+                else
+                {
+                    currentStage++;
+                    picHangman.Image = hangmanImages[currentStage];
+
+                    if (currentStage == 6)
+                    {
+                        MessageBox.Show("You lost! The correct word was " + correctWord);
+                        gameEnded = true;
+                        Lscore += 1;
+                        lblNewWord.Text = "Losses: " + Lscore.ToString();
+                       
+                        return;
+                    }
+                }
+            }
+
+            txtGuess.Text = "";
+        }
+
+
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            score = 0;
+            Lscore = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblScore_TextChanged(object sender, EventArgs e)
+        {
+            lblScore.Text = "Win Count: " + score.ToString();
+
+        }
+
+        private void lblNewWord_TextChanged(object sender, EventArgs e)
+        {
+            lblNewWord.Text = "Loss count: " + Lscore.ToString();
+        }
+        
+
+        private void cmbWordFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            StartNewGame();
+
+        }
+
+        
+    }
+}
